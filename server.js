8c67b4ed31ffc9836ce0a544d7d4cf61c118314f
terribledevice <longlost@gmail.com>
2020-12-05 13:33:50 -0500

    simple crud app using node.js, express, and postgres

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+
+
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require("./models");
+
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+});
+
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to bezkoder application." });
+});
+
+require("./routes/turorial.routes")(app);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
